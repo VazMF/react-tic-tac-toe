7e3358b3e3de2a6d8364fb151bb6b814f902760a
VazMF <vazfernandam@gmail.com>
2025-10-16 01:13:54 -0300

    feat: add turns logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((currentActivePlayer) =>
       currentActivePlayer === "X" ? "O" : "X"
     );
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+      let latestTurn = prevTurns[0];
+
+      if (prevTurns.length > 0 && latestTurn.playerSymbol === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        {
+          square: { row: rowIndex, col: colIndex },
+          playerSymbol: currentPlayer,
+        },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -26,12 +46,9 @@ function App() {
             isActive={activePlayer === "O" ? true : false}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
