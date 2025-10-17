21ed452eec7ea6262e1bb94ad205725e5ede6f27
VazMF <vazfernandam@gmail.com>
2025-10-16 23:05:11 -0300

    feat: add log logic

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns}/>
     </main>
   );
 }
